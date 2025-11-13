7f4fb6b8876dfd536c9590a038dba8fa661e7ce3
Sameerali0 <sameer.ali.professional@gmail.com>
2025-11-13 13:08:19 +0500

    add astronaut movement with arrow keys

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 const gameArea= document.getElementById("game")
 const player= document.getElementById("player")
 
+let playerX= gameArea.clientWidth* 0.1
+let playerY= gameArea.clientHeight / 2
+
+const speed = 6
+const keys = {}
+
+document.addEventListener("keydown", e =>keys[e.key.toLowerCase()] = true)
+document.addEventListener("keyup", e =>keys[e.key.toLowerCase()]= false)
+
+function playerMovement (){
+    if(keys["arrowup"]) playerY -= speed
+    if(keys["arrowdown"]) playerY += speed
+
+    if(keys["arrowleft"])playerX -= speed
+    if(keys["arrowright"])playerX += speed
+
+
+    player.style.left= playerX +"px"
+    player.style.top= playerY +"py"
+
+    requestAnimationFrame(playerMovement)
+
+}
+
+playerMovement()
